7ef83755ccb7214b8c55276148e7acb49bd9c6b1
Sviatoslav <sviatoslav@MacBook-Pro.local>
2025-04-13 19:01:57 +0200

    Ball and Circles game

--- Program.cs
+++ Program.cs
@@ -1,53 +1,121 @@
 ﻿using Raylib_cs;
-using System.Text;
+using System;
 
-class Program
+namespace ConsoleApp1
+{
+    class Program
     {
-
         static void Main()
         {
-            //initialized RayLib window 
-            Raylib.InitWindow(400, 400, "Raylib Test Window");
-            //create input window 
-            StringBuilder userInput = new StringBuilder();
-            int maxLength = 20;
-            Rectangle inputBox = new Rectangle(100, 250, 200, 40);
-            // start ball position
-            float circleX = 400;
-            float circleY = 400;
-            float radius = 30;
-            //target fps in window 
+            Raylib.InitWindow(1000, 800, "Endless Ball Bouncing Game");
             Raylib.SetTargetFPS(60);
-            //loop for raylib window 
+
+            int numCircles = 4;
+            float[] circleRadii = { 150, 200, 250, 300 };
+            float rotationAngle = 0; 
+            float rotationSpeed = 2; 
+            float gapWidth = MathF.PI / 6; 
+
+            float ballX = 500, ballY = 400;
+            float ballRadius = 20;
+            float ballSpeedX = 3, ballSpeedY = 5; 
+            float gravity = 0.2f; 
+            float groundY = 780; 
+            Random random = new Random(); 
+            int currentCircle = 0; 
+
             while (!Raylib.WindowShouldClose())
             {
-                //handle keyboard input
-                int key = Raylib.GetKeyPressed();
-                //handle wrong input
-                if (key != 0 && userInput.Length < maxLength)
+                rotationAngle += rotationSpeed * (MathF.PI / 180); 
+                if (rotationAngle > MathF.PI * 2) rotationAngle -= MathF.PI * 2; 
+
+                ballSpeedY += gravity;
+
+                ballX += ballSpeedX;
+                ballY += ballSpeedY;
+
+                if (ballY + ballRadius >= groundY)
                 {
-                    userInput.Append((char)key);
+                    ballY = groundY - ballRadius; 
+                    ballSpeedY = -5;
+                    ballSpeedX = random.Next(0, 2) == 0 ? 3 : -3; 
                 }
-                //for backspace 
-                if (Raylib.IsKeyPressed(KeyboardKey.Backspace) && userInput.Length > 0)
+
+                if (ballY - ballRadius <= 0)
                 {
-                    userInput.Remove(userInput.Length - 1, 1);
+                    ballY = ballRadius; 
+                    ballSpeedY = 5;
                 }
-                //get cursor postition for ball position
-                circleX = Raylib.GetMouseX();
-                circleY = Raylib.GetMouseY();
-                //draw
+
+                if (ballX - ballRadius <= 0 || ballX + ballRadius >= 1000)
+                {
+                    ballSpeedX = -ballSpeedX; 
+                }
+
+                float circleX = 500, circleY = 400;
+
+                float distX = ballX - circleX;
+                float distY = ballY - circleY;
+                float distance = MathF.Sqrt(distX * distX + distY * distY);
+
+                if (distance + ballRadius > circleRadii[currentCircle])
+                {
+                    float ballAngle = MathF.Atan2(distY, distX);
+                    if (ballAngle < 0) ballAngle += MathF.PI * 2; 
+
+                    float gapStart = rotationAngle;
+                    float gapEnd = rotationAngle + gapWidth;
+
+                    if (ballAngle >= gapStart && ballAngle <= gapEnd)
+                    {
+                        if (currentCircle < circleRadii.Length - 1)
+                        {
+                            currentCircle++; 
+                        }
+                        else
+                        {
+                            currentCircle = 0; 
+                            ballX = 500;
+                            ballY = 400;
+                            ballSpeedX = random.Next(-3, 3); 
+                            ballSpeedY = -5; 
+                        }
+                    }
+                    else
+                    {
+                        ballSpeedY = -ballSpeedY; 
+                        ballSpeedX = -ballSpeedX; 
+                    }
+                }
+
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.White);
-                //input box
-                Raylib.DrawRectangleRec(inputBox, Color.Blue);
-                Raylib.DrawText(userInput.ToString(), (int)inputBox.X+10, (int)inputBox.Y+10, 20, Color.Black);
-                //ball
-                Raylib.DrawCircle((int)circleX, (int)circleY, radius, color:Color.Black);
+
+                for (int i = currentCircle; i < numCircles; i++)
+                {
+                    float gapStartAngle = rotationAngle;
+                    float gapEndAngle = rotationAngle + gapWidth;
+
+                    for (float angle = 0; angle < MathF.PI * 2; angle += 0.01f) 
+                    {
+                        if (angle < gapStartAngle || angle > gapEndAngle) 
+                        {
+                            float startX = circleX + circleRadii[i] * MathF.Cos(angle);
+                            float startY = circleY + circleRadii[i] * MathF.Sin(angle);
+                            float endX = circleX + circleRadii[i] * MathF.Cos(angle + 0.01f);
+                            float endY = circleY + circleRadii[i] * MathF.Sin(angle + 0.01f);
+
+                            Raylib.DrawLine((int)startX, (int)startY, (int)endX, (int)endY, Color.Red);
+                        }
+                    }
+                }
+
+                Raylib.DrawCircle((int)ballX, (int)ballY, ballRadius, Color.Blue);
+
                 Raylib.EndDrawing();
             }
+
             Raylib.CloseWindow();
         }
     }
-
-
+}
